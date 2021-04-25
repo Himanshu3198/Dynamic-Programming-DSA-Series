1f8922487b55664ee3d41fda5d0b63dd481a4979
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-04-25 20:56:13 +0530

    Update DP-knapsackZeroOne.cpp

--- DP-knapsackZeroOne.cpp
+++ DP-knapsackZeroOne.cpp
@@ -1,34 +1,32 @@
 
-
-#include <stdio.h> 
+//  0/1 knapsack problem 
+#include <bits/stdc++.h> 
 using namespace std;
   
 
-int max(int a, int b) 
-{ 
-    return (a > b) ? a : b; 
-} 
-  
 
 int knapSack(int W, int wt[], int val[], int n) 
 { 
-    int i, w; 
-    int K[n + 1][W + 1];  
-    for (i = 0; i <= n; i++) { 
-        for (w = 0; w <= W; w++) { 
-            if (i == 0 || w == 0) 
-                K[i][w] = 0; 
-            else if (wt[i - 1] <= w) 
-                K[i][w] = max( 
-                    val[i - 1] + K[i - 1][w - wt[i - 1]], 
-                    K[i - 1][w]); 
-            else
-                K[i][w] = K[i - 1][w]; 
-        } 
-    } 
+     
 
-  
-    return K[n][W]; 
+        int dp[n+1][W+1];
+        memset(dp,0,sizeof(dp));
+
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=W;j++){
+
+                if(wt[i-1]>j){
+
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]= max(dp[i-1][j],dp[i-1][j-wt[i-1]]+val[i-1]);
+                }
+            }
+        }
+
+        return dp[n][W];
 } 
   
 int main() 
