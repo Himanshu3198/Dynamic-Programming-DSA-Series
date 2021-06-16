b3ded45851e446394f3df321d68f40b734018b30
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-06-16 19:48:08 +0530

    Create DP-divisor game.cpp

---
+++ DP-divisor game.cpp
@@ -0,0 +1,38 @@
+// 1025. Divisor Game
+
+
+class Solution {
+public:
+    int dp[10001];
+    
+    int solve(int n){
+        
+        
+        if(n==1) return 0;
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        for(int i=1;i*i<n;i++){
+            
+            if(n%i==0){
+                if(solve(n-i)==0){
+                    return dp[n]=1;
+                }
+            }
+        }
+        
+        return dp[n]=0;
+    }
+    bool divisorGame(int n) {
+        memset(dp,-1,sizeof dp);
+        
+     
+   
+          return solve(n);
+        
+        
+        
+    }
+};
