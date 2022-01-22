5c925609d991e0e288636adbd6358d470dca67fb
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-01-22 16:35:13 +0530

    Update DP-UnboundedKnapsack.cpp

--- DP-UnboundedKnapsack.cpp
+++ DP-UnboundedKnapsack.cpp
@@ -1,41 +1,70 @@
-// Knapsack with Duplicate Items 
-
-
 #include <bits/stdc++.h>
 using namespace std;
-// User function Template for C++
 
-class Solution{
-public:
-    int knapSack(int n, int W, int val[], int wt[])
-    {
-        // code here
-        int dp[W+1];
-        memset(dp,0,sizeof(dp));
-        
-        
-        
-        
-        for(int i=1;i<=W;i++){
+
+        // memoization
+
+          int dp[1001][1001];
+
+        int solve(int n ,int W,int val[],int wt[],int idx){
             
-            for(int j=0;j<n;j++){
-                
-                
-                if(wt[j]<=i){
-                    
-                   
-                
             
-                    dp[i]=max(dp[i],dp[i-wt[j]]+val[j]);
-                }
+            
+            if(idx>=n or W==0){
+                return 0;
             }
+            
+            if(dp[idx][W]!=-1){
+                return dp[idx][W];
+            }
+            
+            if(wt[idx]<=W){
+                
+                return dp[idx][W]=max(val[idx]+solve(n,W-wt[idx],val,wt,idx),solve(n,W,val,wt,idx+1));
+            }else{
+                
+                return dp[idx][W]=solve(n,W,val,wt,idx+1);
+            }
+            
+            
+            
+            
         }
-      
-        return dp[W];
+    
+    // Tabulation
+    int Tabulation(int n,int W,  int val[],int wt[]){
+         int dp[n+1][W+1];
+         
+         
+         for(int i=0;i<=n;i++){
+             
+             for(int j=0;j<=W;j++){
+                 
+                 if(i==0 or j==0){
+                     dp[i][j]=0;
+                 }else if(wt[i-1]<=j){
+                     
+                     dp[i][j]=max(dp[i-1][j],val[i-1]+dp[i][j-wt[i-1]]);
+                 }else{
+                     dp[i][j]=dp[i-1][j];
+                 }
+             }
+         }
+         
+         
+         for(int i=0;i<=n;i++){
+             
+             for(int j=0;j<=W;j++){
+                 
+                 cout<<dp[i][j]<<" ";
+             }
+             cout<<"\n";
+         }
+         
+         return dp[n][W];
+
     }
-};
-
-
+ 
 
 int main(){
     int t;
@@ -48,9 +77,12 @@ int main(){
             cin>>val[i];
         for(int i = 0;i < N;i++)
             cin>>wt[i];
+
+            //     memset(dp,-1,sizeof(dp));
+    //   return solve(N,W,val,wt,0);
+
         
-        Solution ob;
-        cout<<ob.knapSack(N, W, val, wt)<<endl;
+       
     }
     return 0;
 }  
