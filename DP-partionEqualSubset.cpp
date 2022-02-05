9f8dc07ea77bf7ea5ddea0957a3bbb14e79d7623
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-02-05 15:22:03 +0530

    Update DP-partionEqualSubset.cpp

--- DP-partionEqualSubset.cpp
+++ DP-partionEqualSubset.cpp
@@ -1,51 +1,51 @@
-class Solution {
-    
 
- 
+//  time and space complexity: O(n*sum)
+class Solution {
+   
 public:
     
-       bool canPartition(vector<int>& nums) {
-       
-         int n=nums.size();
-            int sum=0;
-           for(int x:nums){
-               
-               sum+=x;
-           }
-           
-           if(sum%2!=0){
-               return false;
-           }
-           
-            sum/=2;
-           
-           
-           int dp[n+1][sum+1];
-           
-            memset(dp,0,sizeof(dp));
-           
-
-           dp[0][0]=1;
-           
-           for(int i=1;i<=n;i++){
-                 for(int j=1;j<=sum;j++){
-                     
-                 
-                     
-                      if(nums[i-1]>j){
-                              dp[i][j]=dp[i-1][j];
-                          
-                          
-                      }
-                     
-                     else{
-                         dp[i][j]=max(dp[i-1][j],dp[i-1][j-nums[i-1]]);
-                     }
-                 }
-           }
-           
-           return dp[n][sum];
-       }
+        int equalPartition(int n, int arr[])
+    {
+        // code here
+        
+        int sum=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        
+        if(sum%2){
+            return 0;
+        }
+        sum/=2;
+        
+        int dp[n+1][sum+1];
+        memset(dp,0,sizeof(dp));
+        
+        for(int j=1;j<=sum;j++){
+            dp[0][j]=0;
+        }
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=sum;j++){
+                
+                
+                if(arr[i-1]>j){
+                  dp[i][j]=  dp[i-1][j];
+                }else{
+                    
+                    dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum];
+    }
 
     
    
