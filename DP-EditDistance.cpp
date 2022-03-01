e95087370793302e1636932fbcba637682c8ba53
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-03-02 01:52:05 +0530

    Update DP-EditDistance.cpp

--- DP-EditDistance.cpp
+++ DP-EditDistance.cpp
@@ -1,46 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-
-	int min(int x,int y,int z)
-	{
-		return min(min(x,y),z);
-	}
-
-
-	int EditDistance(string str1,string str2){
-		int m=str1.length();
-		int n=str2.length();
-
-		int dp[m+1][n+1];
-
-		for(int i=0;i<=m;i++)
-		{
-			for(int j=0;j<=n;j++){
-				if(i==0)
-				dp[i][j]=j;
-
-				else if(j==0)
-				dp[i][j]=i;
-
-				else if(str1[i-1]==str2[j-1])
-				{
-					dp[i][j]=dp[i-1][j-1];
-				}
-
-				else{
-					dp[i][j]=1+ min(  dp[i-1][j-1],dp[i-1][j],dp[i][j-1] );
-				}
-
-			}
-		}
-
-		return dp[m][n];
-	}
-	int main()
-{
-	string str1 = "sunday";
-	string str2 = "saturday";
-
-	cout << "\nmin no of operation needed to convert string one to another\n"<<EditDistance(str1, str2) << "\n";
-}
+//   time compelxity O(n*m)
+int n=s.size(),m=t.size();
+         
+         
+          int dp[n+1][m+1];
+          
+           memset(dp,0,sizeof(dp));
+           
+           for(int i=0;i<=n;i++){
+               
+               dp[i][0]=i;
+           }
+           
+           for(int j=0;j<=m;j++){
+               
+               dp[0][j]=j;
+           }
+           
+            for(int i=1;i<=n;i++){
+                
+                for(int j=1;j<=m;j++){
+                    
+                    
+                   if(s[i-1]==t[j-1]){
+                       dp[i][j]=dp[i-1][j-1];
+                   }else{
+                       
+                       dp[i][j]=1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
+                   }
+                }
+               
+            }
+            
+    
+           return dp[n][m];
+           
+           
+        
+    }
