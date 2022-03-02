fd3d4cd58c0a0f2106502843127749847ed578ed
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-03-02 20:06:48 +0530

    Update DP-maximizeCutSegment.cpp

--- DP-maximizeCutSegment.cpp
+++ DP-maximizeCutSegment.cpp
@@ -1,29 +1,41 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define mod 1000000007
+  
+//  time complexity O(n*m)
+ int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        //Your code here
+        
+        vector<int>coin={x,y,z};
+     
+            sort(coin.begin(),coin.end());
+           int dp[n+1];
+         
 
-int main()
- {
-	int t ;
-	cin >> t ;
-	while(t--){
-	    int n , i , step[3] , j ;
-	    cin >> n ;
-	    cin >> step[0] >> step[1] >> step[2] ;
-	    sort(step , step+3) ;
-	    int dp[n+1] ;
-	    for(i=0;i<n+1;i++){
-	        dp[i] = INT_MIN ;
-	    }
-	    dp[0] = 0 ;
-	    for(int i=1;i<=n;i++)
-	    {
-	        for(int j=0;j<3;j++)
-	            if(step[j]<=i)
-	                dp[i]=max(dp[i],dp[i-step[j]]+1);
-	    }
-	    cout << dp[n] << endl ;
-	}
-	
-	return 0;
-}
+         for(int i=0;i<=n;i++){
+             
+             dp[i]=INT_MIN;
+         }
+         
+         dp[0]=0;
+         
+         
+         
+         for(int i=1;i<=n;i++){
+             
+             for(int x:coin){
+                 if(x<=i)
+                  dp[i]=max(dp[i],(dp[i-x]+1));
+             }
+         }
+         
+        if(dp[n]<0){
+            return 0;
+        }
+        
+        return dp[n];
+         
+      
+     
+        
+      
+      
+    }
