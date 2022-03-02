ad3e9f51b06cb2d08ab564a202a0add9d830ed91
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-03-02 20:43:38 +0530

    Update and rename DP-longestPalindromicSubsequence.cpp to LCS-longestPalindromicSubsequence.cpp

--- LCS-longestPalindromicSubsequence.cpp
+++ LCS-longestPalindromicSubsequence.cpp
@@ -1,37 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-
- void longestPalinseq (string s)
-{
-    // your code here
-       string t=s;
-          int n=s.length();
-          reverse(s.begin(),s.end());
+    int longestPalinSubseq(string A) {
+        //code here
+        //  time complexity o(n*n)
+        //  space complexity o(n)
+        string B=A;
+        reverse(B.begin(),B.end());
         
         
-          int dp[n+1][n+1];
+        int n=A.size();
+        
+        int dp[n+1][n+1];
+        
         memset(dp,0,sizeof(dp));
         
-         
+        
         for(int i=1;i<=n;i++){
             
             for(int j=1;j<=n;j++){
                 
-                if(s[i-1]==t[j-1]){
+                
+                if(A[i-1]==B[j-1]){
                     
                     dp[i][j]=1+dp[i-1][j-1];
-                }
-                else {
+                }else{
+                    
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
         
-         cout<<dp[n][n]<<"\n";
-    
-  
+        return dp[n][n];
         
-}
+        
+        
+    }
 int main()
 {
     int t; cin >> t;
@@ -39,7 +42,7 @@ int main()
     {
         string S; cin >> S;
 
-         longestPalinseq (S);
+        cout<< longestPalinSubseq(S);
     }
 }
 
