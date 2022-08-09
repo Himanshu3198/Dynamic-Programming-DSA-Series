5a351a9f3e9486c244b84605935b907e2604a731
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-08-09 14:10:32 +0530

    Update memo+tabulation-1220. Count Vowels Permutation.cpp

--- memo+tabulation-1220. Count Vowels Permutation.cpp
+++ memo+tabulation-1220. Count Vowels Permutation.cpp
@@ -23,3 +23,37 @@ public:
         return result;
     }
 };
+
+// tabulation 
+class Solution {
+public:
+     const int mod=1e9+7;
+    int countVowelPermutation(int n) {
+       
+     
+         // 0 1 2 3 4
+         // a e i o u
+        long long dp[n][5];
+        for(int i=0;i<5;i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<n;i++){
+            // presendence
+            // a -> e i u
+            dp[i][0]=(dp[i-1][1]+dp[i-1][2]+dp[i-1][4])%mod;
+            // e-> a i
+            dp[i][1]=(dp[i-1][0]+dp[i-1][2])%mod;
+            // i-> e o
+            dp[i][2]=(dp[i-1][1]+dp[i-1][3])%mod;
+            // o->i
+            dp[i][3]=(dp[i-1][2])%mod;
+            // u-> i o
+            dp[i][4]=(dp[i-1][2]+dp[i-1][3])%mod;
+            
+        }
+        
+        int ans=0;
+        for(int i=0;i<5;i++) ans=(ans%mod+dp[n-1][i]%mod)%mod;
+        return ans;
+    }
+};
