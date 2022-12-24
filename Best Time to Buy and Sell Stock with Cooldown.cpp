0a420b66d54e5752062c3cd2bf06f8d0600ae2f9
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-12-24 19:20:55 +0530

    Update Best Time to Buy and Sell Stock with Cooldown.cpp

--- Best Time to Buy and Sell Stock with Cooldown.cpp
+++ Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -1,49 +1,32 @@
-// 309. Best Time to Buy and Sell Stock with Cooldown.cpp
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-     
-           int obsp=-prices[0];
-           int ossp=0;
-           int ocsp=0;
-        
-        
-        for(int i=1;i<prices.size();i++){
+    int dp[5001][3];
+    int solve(vector<int>&prices,int status,int idx){
+        int buyThis=0,notBuy=0;
+        if(idx>=prices.size()) return 0;
+        if(dp[idx][status]!=-1) return dp[idx][status];
+//         I  don't want to buy this stock
+        notBuy=solve(prices,status,idx+1);
+        if(status==0){
+//            I want to buy this stock 
+            buyThis=-prices[idx]+solve(prices,1,idx+1);
+        }else if(status==1){
             
-            
-            int nbsp=0;
-            int nssp=0;
-            int ncsp=0;
-            
-            
-            if(ocsp-prices[i]>obsp){
-                
-                nbsp=ocsp-prices[i];
-            }else{
-                nbsp=obsp;
-            }
-            
-            
-            if(obsp+prices[i]>ossp){
-                
-                nssp=obsp+prices[i];
-            }else{
-                
-                nssp=ossp;
-            }
-            
-            if(ossp>ocsp){
-                ncsp=ossp;
-            }else{
-                
-                ncsp=ocsp;
-            }
-            
-            obsp=nbsp;
-            ossp=nssp;
-            ocsp=ncsp;
+//             if I have bought  stock then left with two option 
+//             first - I don't want to sell this stock on current day so move to next day
+//             second - I want to sell stock ,so sell and move to 2 days ahead
+               
+               int first=solve(prices,status,idx+1);
+               int second=prices[idx]+solve(prices,0,idx+2);
+               buyThis=max(first,second);
         }
+        return dp[idx][status]=max(notBuy,buyThis);
         
-        return ossp;
+    }
+    int maxProfit(vector<int>& prices) {
+          memset(dp,-1,sizeof(dp));
+          // 0 means I want to buy this stock
+          // 1 means I have already bought this stock
+          return solve(prices,0,0);
     }
 };
