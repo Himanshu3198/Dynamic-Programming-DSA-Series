9421bd9f5f59da9ed9771f64523d69c77e783e8a
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2023-08-05 13:14:40 +0530

    Create Lcs-memoization-tabulation-Minimum ASCII Delete Sum for Two Strings.cpp

---
+++ Lcs-memoization-tabulation-Minimum ASCII Delete Sum for Two Strings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+   
+    int dp[1001][1001];
+    int solve(string &s1,string &s2,int i,int j,unordered_map<char,int>&mp){
+        
+        if(i==s1.size() || j==s2.size()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int curr=0;
+        if(s1[i]==s2[j]){
+            
+            curr=mp[s1[i]]+solve(s1,s2,i+1,j+1,mp);
+        }else{
+            curr=max(solve(s1,s2,i,j+1,mp),solve(s1,s2,i+1,j,mp));
+        }
+        return dp[i][j]=curr;   
+    }
+    int minimumDeleteSum(string s1, string s2) {
+       
+        memset(dp,-1,sizeof(dp));
+        int tot=0;
+         unordered_map<char,int>mp;
+        int  ptr=97;
+    
+        for(char c='a';c<='z';c++){
+            mp[c]=ptr++;
+        }
+        ptr=65;
+        for(char c='A';c<='Z';c++){
+            mp[c]=ptr++;
+        }
+        for(auto &it:s1) tot+=(mp[it]);
+        for(auto &it:s2) tot+=(mp[it]);
+        int res=tot-2*solve(s1,s2,0,0,mp);
+        return res;
+        
+    }                   
+};
