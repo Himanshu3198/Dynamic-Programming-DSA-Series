68bdeb3af3856e0a55f6497797ccedb52998cdd0
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2023-08-19 18:49:07 +0530

    Update Lcs-memoization-tabulation-Minimum ASCII Delete Sum for Two Strings.cpp

--- Lcs-memoization-tabulation-Minimum ASCII Delete Sum for Two Strings.cpp
+++ Lcs-memoization-tabulation-Minimum ASCII Delete Sum for Two Strings.cpp
@@ -37,3 +37,38 @@ public:
         
     }                   
 };
+
+// ***********tabuluation**********************
+int minimumDeleteSum(string s1, string s2) {
+       
+        memset(dp,0,sizeof(dp));
+        int tot=0,n=s1.size(),m=s2.size();
+        unordered_map<char,int>mp;
+        int  ptr=97;
+    
+        for(char c='a';c<='z';c++){
+            mp[c]=ptr++;
+        }
+        ptr=65;
+        for(char c='A';c<='Z';c++){
+            mp[c]=ptr++;
+        }
+        for(auto &it:s1) tot+=(mp[it]);
+        for(auto &it:s2) tot+=(mp[it]);
+          
+         for(int i=n-1;i>=0;i--){
+             for(int j=m-1;j>=0;j--){
+                 int curr=0;
+                 if(s1[i]==s2[j]){
+                     curr=mp[s1[i]]+dp[i+1][j+1];
+                 }else{
+                     curr=max(dp[i][j+1],dp[i+1][j]);
+                 }
+                 dp[i][j]=curr;
+             }
+         }
+        int res=tot-2*dp[0][0];
+        return res;
+        
+    }                   
+};
