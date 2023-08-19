8e13e9f505effecb187de564a43c70670ffd805c
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2023-08-19 23:25:13 +0530

    Update memoization+tabulation-2786. Visit Array Positions to Maximize Score.cpp

--- memoization+tabulation-2786. Visit Array Positions to Maximize Score.cpp
+++ memoization+tabulation-2786. Visit Array Positions to Maximize Score.cpp
@@ -30,3 +30,29 @@ public:
        
     }
 };
+
+// *******tabulation******************************
+long long maxScore(vector<int>& nums, int x) {
+        
+        memset(dp,0,sizeof(dp));
+        int parity=nums[0]%2;
+        // return solve(nums,x,1,nums[0],parity)+nums[0];
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            for(int parity=0;parity<=1;parity++){
+                long long ans=0;
+                int newParity=nums[i]%2,fine=0;
+                if(newParity!=parity)
+                  fine=x;
+                long long op1=nums[i]-fine+dp[i+1][newParity];
+                long long op2=dp[i+1][parity];
+                ans=Max(op1,op2);
+                dp[i][parity]=ans;
+            }
+            
+        }
+        return dp[0][parity];
+        
+       
+    }
+};
