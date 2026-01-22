29609dd00076f40a1c0bafcea109751d0ee58687
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2026-01-23 00:17:16 +0530

    Implement combinationSum4 in Python
    Added Python implementation of combination sum IV algorithm.

--- Knapsack-377. Combination Sum IV.cpp
+++ Knapsack-377. Combination Sum IV.cpp
@@ -16,3 +16,21 @@ public:
         return memo(nums,0,target);
     }
 };
+
+
+// python code
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+
+        dp = {}
+        dp[0] = 1
+
+        for t in range(1,target+1):
+            dp[t] = 0
+            for n in nums:
+                if t >= n:
+                   dp[t] = dp[t]+dp[t-n]
+
+        return dp[target] 
+        
